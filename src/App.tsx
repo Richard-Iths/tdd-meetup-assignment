93759b281c55354e3bc371c88bf717d87ef14005
richard-iths <richard.forsberg@iths.se>
2021-12-13 17:56:15 +0100

    init commit with some sass utils

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./app.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="xxl">EXTRA LARGE</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
