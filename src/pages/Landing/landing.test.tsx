773a7879f3fcf002aa924452929b1142428576ca
richard-iths <richard.forsberg@iths.se>
2021-12-14 14:18:59 +0100

    added recoil state for events

--- src/pages/Landing/landing.test.tsx
+++ src/pages/Landing/landing.test.tsx
@@ -1,11 +1,11 @@
 import { render } from '@testing-library/react';
-import App from './App';
+import LandingPage from './Landing';
 import React from 'react';
 
 describe('Landing.tsx', () => {
   describe('Smoke tests', () => {
     it('Should render landing page', () => {
-      render(<App />);
+      render(<LandingPage />);
     });
   });
 });
