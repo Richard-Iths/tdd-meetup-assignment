5f2345e25da85ec2c1d44be45b50c95ce0f666de
richard-iths <richard.forsberg@iths.se>
2021-12-15 10:04:28 +0100

    started on user settings component and tests

---
+++ src/components/user/settings/UserSettings.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserSettings: React.FC = ({}) => {
+  return (
+    <header className="app-header">
+      <h2 data-test="header-logo">Awesome Meetups</h2>
+    </header>
+  );
+};
+
+export default UserSettings;
